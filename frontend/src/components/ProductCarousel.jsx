7206476cfe158aa3162753b6beba6a274bc2045a
Shakhlyn <shakhlyn.sh.du@gmail.com>
2023-11-14 10:59:19 +0600

    fix: Add link to the carousel's product name
    - Now users can easily go to the product page after clicking the product of carousel.

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,70 +1,3 @@
-// import { Link } from "react-router-dom";
-// import Message from "./message";
-// import { useGetTopProductsQuery } from "../slices/productsApiSlice";
-// import { useState } from "react";
-// import {
-//   BsFillArrowRightCircleFill,
-//   BsFillArrowLeftCircleFill,
-// } from "react-icons/bs";
-
-// export default function ProductCarousel() {
-//   const { data, isLoading, error } = useGetTopProductsQuery();
-
-//   let [current, setCurrent] = useState(0);
-
-//   let previousSlide = () => {
-//     if (current === 0) setCurrent(data?.data?.length - 1);
-//     else setCurrent(current - 1);
-//   };
-
-//   let nextSlide = () => {
-//     if (current === data?.data?.length - 1) setCurrent(0);
-//     else setCurrent(current + 1);
-//   };
-
-//   return (
-//     <div className="overflow-hidden relative">
-//       {/* <div className="grid grid-cols-12"> */}
-//       <div className=" max-h-60 ">
-//         <img
-//           src={data?.data[current]?.image}
-//           alt="product image"
-//           className=" mx-auto w-1/3 max-h-60 "
-//         />
-//       </div>
-//       <div></div>
-//       {/* </div> */}
-
-//       <div className="absolute top-0 h-full w-2/3 justify-between items-center flex text-darkGray px-10 text-3xl">
-//         <button onClick={previousSlide}>
-//           <BsFillArrowLeftCircleFill />
-//         </button>
-//         <button onClick={nextSlide}>
-//           <BsFillArrowRightCircleFill />
-//         </button>
-//       </div>
-
-//       <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
-//         {data?.data?.map((product, index) => {
-//           return (
-//             <div
-//               onClick={() => {
-//                 setCurrent(index);
-//               }}
-//               key={"circle" + index}
-//               className={`rounded-full w-5 h-5 cursor-pointer  ${
-//                 index == current ? "bg-gray-200" : "bg-gray-500"
-//               }`}
-//             ></div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// *********************************************************************************************
-
 import { Link } from "react-router-dom";
 import Message from "./message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
@@ -102,15 +35,16 @@ export default function ProductCarousel() {
           className=" col-span-6 mobile:h-28 md:h-40 mx-auto transition-transform duration-3000 transform hover:scale-105"
         />
 
-        {/* <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex flex-col items-center text-center"> */}
-        <div className=" col-span-6 hover:scale-105">
+        <div className="col-span-6">
           <h3 className="text-lg font-semibold mb-2 mobile:pt-2 md:pt-10">
-            {data?.data[current]?.name}
+            <Link to={`/products/${data.data[current]._id}`}>
+              {data?.data[current]?.name}
+            </Link>
           </h3>
+
           <p className="text-gray-500">${data?.data[current]?.price} Only!!!</p>
         </div>
       </div>
-
       <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between items-center px-4">
         <button
           onClick={previousSlide}
