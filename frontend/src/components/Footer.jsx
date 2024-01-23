0d97d8b632cc46c935e6095e8823a9e009f147bd
Shakhlyn <shakhlyn.sh.du@gmail.com>
2024-01-23 15:59:11 +0600

    fix: Update footer style so that the two parts remains consistant

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,12 +5,14 @@ const Footer = () => {
   return (
     <footer className="mt-auto">
       <div className="flex justify-center bg-darkGray text-slate-100 py-4 ">
-        {/* &copy; Agora 2023. All rights reserved. */}
-        &copy; {currentYear} Agora 2023. All rights reserved | Designed and
-        developed by <span>&nbsp;</span>
-        <a href={developer_link}>
-          <span className="hover:text-yellow-500 ">Shakhlyn</span>
-        </a>
+        <p>
+          {/* &copy; Agora 2023. All rights reserved. */}
+          &copy; {currentYear} Agora 2023. All rights reserved | Designed and
+          developed by <span>&nbsp;</span>
+          <a href={developer_link}>
+            <span className="hover:text-yellow-500 ">Shakhlyn</span>
+          </a>
+        </p>
       </div>
     </footer>
   );
